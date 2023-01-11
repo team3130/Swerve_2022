7b5647ea06729f2020c4dbeb798d1896ef59af8c
Caleb <calebkugel1@gmail.com>
2023-01-10 20:45:18 -0600

    swerve module setup

--- src/main/java/frc/robot/swerve/SwerveModule.java
+++ src/main/java/frc/robot/swerve/SwerveModule.java
@@ -1,5 +1,17 @@
 package frc.robot.swerve;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+
 public class SwerveModule {
+    private final TalonFX angley;
+    private final TalonFX spinny;
+
+    private final CANCoder driveEncoder;
+
+    public void SwerveModule(int ) {
+
+    }
+
 
 }
