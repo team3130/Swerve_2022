87a6329399bd99126fa23f69ff36c39c96e12631
TheFishyCoder <102122425+TheFishyCoder@users.noreply.github.com>
2023-01-30 21:40:47 -0600

    Update#2
    Code Broke Again

--- src/main/java/frc/robot/sensors/Limelight.java
+++ src/main/java/frc/robot/sensors/Limelight.java
@@ -1,14 +1,13 @@
 package frc.robot.sensors;
 
-import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
-import org.photonvision.targeting.TargetCorner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Limelight {
@@ -16,25 +15,45 @@ public class Limelight {
     PhotonCamera camera;
 
     private final GenericEntry ntHasTarget;
+    private final GenericEntry ntYaw;
+    private final GenericEntry ntDifferentTargets;
+    private final GenericEntry ntID;
+
 
     private static ShuffleboardTab tab = Shuffleboard.getTab("PhotonCamera");
-        public Limelight(){
-            camera = new PhotonCamera("OV5647");
-            ntHasTarget = tab.add("HasTarget", false).getEntry();
+
+    public Limelight() {
+        camera = new PhotonCamera("OV5647");
+        ntHasTarget = tab.add("HasTarget", false).getEntry();
+        ntYaw = tab.add("Yaw", 0).getEntry();
+        ntDifferentTargets = tab.add("DifferentTargets", new Long[0]).getEntry();
+        ntID = tab.add("ID", 0).getEntry();
     }
 
-         public void outputToShuffleBoard(){
+    public void outputToShuffleBoard() {
 
-             PhotonPipelineResult result = camera.getLatestResult();
-             PhotonTrackedTarget target = result.getBestTarget();
-             boolean hasTargets = result.hasTargets();
-             double yaw = target.getYaw();
-             double pitch = target.getPitch();
-             double area = target.getPitch();
-             double skew = target.getSkew();
-             int targetID = target.getFiducialId();
+        PhotonPipelineResult result = camera.getLatestResult();
+        PhotonTrackedTarget target = result.getBestTarget();
+        if (target != null) {
+            boolean hasTargets = result.hasTargets();
+            double yaw = target.getYaw();
+            double pitch = target.getPitch();
+            double area = target.getPitch();
+            double skew = target.getSkew();
+            int targetID = target.getFiducialId();
+
+            ntHasTarget.setBoolean(hasTargets);
+            ntYaw.setDouble(target.getYaw());
+            ntID.setInteger(target.getFiducialId());
 
 
-             ntHasTarget.setBoolean(hasTargets);
-         }
+            ArrayList<PhotonTrackedTarget> diffrentID = new ArrayList<>(result.getTargets());
+            Long[] fiducialIDs = new Long[diffrentID.size()];
+
+            for (int index = 0; index < diffrentID.size(); index++) {
+                fiducialIDs[index] = ((long) diffrentID.get(index).getFiducialId());
+
+            }
+        }
+    }
 }
